7554b6e2f7606fcbbdc378f36887545a06a5d910
Argie <argie.b@flowerstore.ph>
2023-01-29 20:58:28 +0800

    add verify otp screen

--- pages/auth/verify.js
+++ pages/auth/verify.js
@@ -1,21 +1,38 @@
 import React, { useState } from 'react'
 
-import OtpInput from 'react-otp-input';
+
+//Components
+import OtpInputs from '../../components/Common/OtpInputs'
+// import OtpInput from 'react-otp-input';
 
 const Verify = () => {
   const [otp, setOTP] = useState(null)
+
+  const handleVerifyOTP = () => {
+    console.log("OTP:", otp) // OTP CODE
+  }
+
   return (
     <div className='h-screen grid place-items-center'>
-      <OtpInput
-        value={otp}
-        onChange={(value) => setOTP(value)}
-        inputStyle={{
-          padding: '2rem',
-          
-        }}
-        numInputs={6}
-        separator={<span>-</span>}
-      />
+      <div className='grid place-items-center gap-12'>
+        <OtpInputs 
+          value={otp}
+          onChange={(value) => setOTP(value)}
+          numInputs={6}
+          inputStyle={{
+            width: '4rem',
+            height: '4rem',
+            fontWeight: 'bold',
+            fontSize: '2rem'
+          }}
+        />
+        <button 
+          className='bg-gray-400 p-4'
+          onClick={handleVerifyOTP}
+        >
+          <label>Verify OTP</label>
+        </button>
+      </div>
     </div>
   )
 }
