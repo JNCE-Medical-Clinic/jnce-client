7ab5cafba495ce69060c2cd9ee1b6d0d396d12cb
Isaac Sarte <sarteisaac09@gmail.com>
2023-07-06 23:18:43 +0800

    added supabase comment

--- services/supabase.js
+++ services/supabase.js
@@ -1,3 +1,4 @@
 import { createClient } from '@supabase/supabase-js';
 
+// supabase credentials
 export const supabase = createClient(process.env.NEXT_PUBLIC_SUPABASE_URL, process.env.NEXT_PUBLIC_SUPABASE_API_KEY);
